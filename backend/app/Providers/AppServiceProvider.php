45595157c62346b290ebae814e7f77a5fd3352a5
Szufli <59046646+Szufli@users.noreply.github.com>
2025-07-10 14:47:50 +0200

    Update AppServiceProvider.php

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -11,18 +11,13 @@ use App\Infrastructure\Persistence\Eloquent\EloquentUploader;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Rejestracja usług kontenera.
-     */
+
     public function register(): void
     {
-        // Bindowanie interfejsów do implementacji
+
         $this->app->bind(AnnouncementRepositoryInterface::class, EloquentAnnouncementRepository::class);
     }
 
-    /**
-     * Bootstrapowanie dowolnych usług aplikacji.
-     */
     public function boot(): void
     {
         //
